d4e520322eeecd85b1d7a29c1f81c5bccee85f8a
Braydon <foxden00@gmail.com>
2017-02-08 16:16:56 -0500

    Add project files.

---
+++ Part 1/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Part_1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Rent today!</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <h1>Vehicle Types and Pricing</h1>
+
+        <p>
+        <asp:Image ID="imgSelectedCar" runat="server" />
+        </p>
+
+        <p>Vehicle Type:&nbsp&nbsp&nbsp
+        <asp:DropDownList ID="ddlVehicleType" runat="server" AutoPostBack="True">
+            <asp:ListItem Value="24.99">Economy</asp:ListItem>
+            <asp:ListItem Value="29.99">Compact</asp:ListItem>
+            <asp:ListItem Value="34.99">Intermediate</asp:ListItem>
+            <asp:ListItem Value="44.99">Standard</asp:ListItem>
+            <asp:ListItem Value="49.99">Full-Size</asp:ListItem>
+        </asp:DropDownList>
+        </p>
+
+        <p>Number of Days:&nbsp&nbsp&nbsp
+            <asp:TextBox ID="tbxNumberOfDays" runat="server" TextMode="Number"></asp:TextBox>
+        </p>
+
+        <p>
+            <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+        </p>
+
+        <p>
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </p>
+
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Part 1/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Part_1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            setCarImage();
+            lblResult.Text = "";
+        }
+
+        private void setCarImage()
+        {
+            switch (ddlVehicleType.SelectedItem.Text)
+            {
+                case "Economy":
+                    imgSelectedCar.ImageUrl = "~/images/economy.png";
+                    break;
+                case "Compact":
+                    imgSelectedCar.ImageUrl = "~/images/compact.png";
+                    break;
+                case "Intermediate":
+                    imgSelectedCar.ImageUrl = "~/images/intermediate.png";
+                    break;
+                case "Standard":
+                    imgSelectedCar.ImageUrl = "~/images/standard.png";
+                    break;
+                case "Full-Size":
+                    imgSelectedCar.ImageUrl = "~/images/full-size.png";
+                    break;
+                default:
+                    imgSelectedCar.Visible = false;
+                    break;
+            }
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            double totalCost = calculateTotalCost();
+            if (totalCost > 0)
+            {
+                lblResult.Text = "Total cost: $" + totalCost;
+            }
+            else
+            {
+                lblResult.Text = "Invalid input";
+            }
+        }
+
+        private double calculateTotalCost()
+        {
+            try
+            {
+                double dailyCharge = double.Parse(ddlVehicleType.SelectedValue);
+                double numberOfDays = double.Parse(tbxNumberOfDays.Text);
+                return dailyCharge * numberOfDays;
+            }
+            catch (Exception)
+            {
+                return -1.0;
+            }
+        }
+    }
+}
