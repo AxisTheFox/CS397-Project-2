5dd65beacb809c16f606e1bfadf8f1ef1de2d50b
Braydon <foxden00@gmail.com>
2017-02-10 16:49:16 -0500

    Implemented Part 2 of project.

---
+++ Part 2/WebForm1.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Part_2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Parking Calculator</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <h1>Parking Cost Calculator</h1>
+
+        <p>Total Hours Parked:&nbsp&nbsp&nbsp
+            <asp:TextBox ID="tbxHoursParked" runat="server"></asp:TextBox>
+        </p>
+
+        <asp:Button ID="btnCalculate" runat="server" Text="Calculate" OnClick="btnCalculate_Click" />
+
+        <h3>
+            <asp:Label ID="lblResult" runat="server" Text=""></asp:Label>
+        </h3>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Part 2/WebForm1.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Part_2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double hoursParked = double.Parse(tbxHoursParked.Text);
+                double totalCost = 5.00;
+                if (hoursParked > 3.00)
+                {
+                    double remainingHours = hoursParked - 3.00;
+                    while (remainingHours >= 1.00)
+                    {
+                        remainingHours -= 1.00;
+                        totalCost += 1.50;
+                    }
+                    if (remainingHours > 0.00)
+                    {
+                        totalCost += 1.50;
+                    }
+                    if (totalCost > 18.00)
+                    {
+                        totalCost = 18.00;
+                    }
+                }
+                lblResult.ControlStyle.ForeColor = System.Drawing.Color.Black;
+                lblResult.Text = "Total Cost: $" + totalCost;
+            }
+            catch (Exception)
+            {
+                lblResult.ControlStyle.ForeColor = System.Drawing.Color.Red;
+                lblResult.Text = "Invalid input.";
+            }
+        }
+    }
+}
