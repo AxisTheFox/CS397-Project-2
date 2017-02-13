76a7dc5166b1387e6f4555f53ef20e6d893b6b6b
Braydon <foxden00@gmail.com>
2017-02-13 17:25:31 -0500

    Completed implementation of Part 3

---
+++ Part 3/WebForm1.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Part_3.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+
+        <p>Select State:<br />
+        <asp:DropDownList ID="ddlStates" runat="server" AutoPostBack="True" OnSelectedIndexChanged="ddlStates_SelectedIndexChanged">
+        </asp:DropDownList>
+        </p><br />
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ Part 3/WebForm1.aspx.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data.OleDb;
+
+namespace Part_3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                FillStatesDropDown();
+            }
+        }
+
+        protected void ddlStates_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            OleDbConnection conn = ConnectToDatabase();
+            OleDbCommand cmd = CreateOleDbCommand("select FirstName, LastName from Information where State=@s", conn);
+            cmd.Parameters.AddWithValue("@s", ddlStates.SelectedValue);
+            conn.Open();
+            OleDbDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                Response.Write(rdr["FirstName"].ToString() + " " + rdr["LastName"].ToString() + "<br/>");
+            }
+            rdr.Close();
+            conn.Close();
+        }
+
+        private void FillStatesDropDown()
+        {
+            OleDbConnection conn = ConnectToDatabase();
+            OleDbCommand cmd = CreateOleDbCommand("select distinct State from Information", conn);
+            conn.Open();
+            OleDbDataReader rdr = cmd.ExecuteReader();
+            while (rdr.Read())
+            {
+                ddlStates.Items.Add(new ListItem(rdr["State"].ToString()));
+            }
+            rdr.Close();
+            conn.Close();
+        }
+
+        private OleDbConnection ConnectToDatabase()
+        {
+            return new OleDbConnection(ConfigurationManager.ConnectionStrings["StudentsConnection"].ToString());
+        }
+
+        private OleDbCommand CreateOleDbCommand(string qry, OleDbConnection conn)
+        {
+            return new OleDbCommand(qry, conn);
+        }
+    }
+}
