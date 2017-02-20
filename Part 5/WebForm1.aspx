0c94a8103f9bca048f46cee3b6b47d0608da23f9
Braydon <foxden00@gmail.com>
2017-02-20 17:32:00 -0500

    Implemented part 5 of project.

---
+++ Part 5/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Part_5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <p>
+                Search by area code:
+                <br />
+                <asp:TextBox ID="tbxZipSearch" runat="server" TextMode="Number"></asp:TextBox>
+                &nbsp&nbsp&nbsp
+                <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+                <br />
+                <br />
+                <asp:GridView ID="gvDisplay" runat="server"></asp:GridView>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Part 5/WebForm1.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Part_5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            OleDbConnection conn = new OleDbConnection(ConfigurationManager.ConnectionStrings["StudentsConnection"].ConnectionString);
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM Information WHERE Zip LIKE @z", conn);
+            cmd.Parameters.AddWithValue("@z", tbxZipSearch.Text);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            gvDisplay.DataSource = dt;
+            gvDisplay.DataBind();
+        }
+    }
+}
